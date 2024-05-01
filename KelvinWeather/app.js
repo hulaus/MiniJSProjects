02e113e1b3821b407c7901f581dd22770b1cf6db
Jo Carr <hulaus21@gmail.com>
2024-04-30 19:52:46 -0700

    added final sections of converting fahrenheit to Newtons and adding a string Interpolation

--- KelvinWeather/app.js
+++ KelvinWeather/app.js
@@ -8,3 +8,15 @@ const celsius = kelvin - 273;
 //I used this formula because it is how you would convert celius to Fahrenheit. I am not sure it is going to work with the division but I will see.
 const fahrenheit = celsius * (9/5) + 32;
 const roundedFahrenheit = Math.floor(fahrenheit);
+
+//I had to look up how Math.floor is properly syntaxed on MDN but once I saw the syntax I put Fahrenheit inside of Math.floor to get the rounded number 
+
+//console.log('The temperature is ' + Math.floor(Fahrenheit) + ' degrees Fahrenheit.');
+
+//Looked at the hint for this part of the project and showed me how to write the code to be cleaner. I previously used string concatenation but it is simpler and cleaner to use string Interpolation.
+console.log(`The temperature is ${Math.floor(roundedFahrenheit)} degrees Fahrenheit.`);
+
+const newton = celsius * (33/100);
+const roundedNewton = Math.floor(newton);
+
+console.log(`If you are looking for the temperature on the Newton scale the temperature is ${roundedNewton} today.`);
