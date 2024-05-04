ff1826bf46a0daf95fc40dbd5268e7e4623e35dd
Jo Carr <hulaus21@gmail.com>
2024-05-03 20:28:27 -0700

    added second part of the code which is finding the later years variable

--- DogYears/dogYears.js
+++ DogYears/dogYears.js
@@ -4,3 +4,15 @@ const myAge = 30;
 
 var earlyYears = 2;
 //I put this variable as let because it will be changing soon
+
+earlyYears = earlyYears * 10.5;
+
+laterYears = myAge - 2;
+
+// I wrote task 5 this way because it seems like this is how I would reassign the laterYears variable to account for mulitplying the variable by 4
+laterYears = laterYears * 4;
+
+console.log(laterYears);
+console.log(earlyYears);
+
+//I made myAgeInDogYears as a const because the formula will not be chaning it will always be ealryYears + laterYears
