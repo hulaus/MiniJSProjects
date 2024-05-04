42d0bd143f46875783a4d370f0f5f3239002771b
Jo Carr <hulaus21@gmail.com>
2024-05-03 20:26:54 -0700

    added the first steps to calculating my age in dog years by setting up my age and saving the earlyYears variable as 2

---
+++ DogYears/dogYears.js
@@ -0,0 +1,6 @@
+// Made variable a const because my age will not be changing for another 2 or 3 months. This will be staying constant while I am making this project
+
+const myAge = 30;
+
+var earlyYears = 2;
+//I put this variable as let because it will be changing soon
